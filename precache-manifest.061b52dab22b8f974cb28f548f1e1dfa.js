23cb25f4996031c0b670b1b6824f48e9791e9f69
Lex Aeterna <lexaeterna12321@gmail.com>
2019-08-17 10:37:24 +0200

    Updates

---
+++ precache-manifest.061b52dab22b8f974cb28f548f1e1dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd53e09d8807d0ae9bd083f2b00300c2",
+    "url": "/dad-jokes-app/index.html"
+  },
+  {
+    "revision": "3f909f4c7b3b30ae1276",
+    "url": "/dad-jokes-app/static/css/main.3ea64c71.chunk.css"
+  },
+  {
+    "revision": "0dbc672e244966d17acb",
+    "url": "/dad-jokes-app/static/js/2.d583786a.chunk.js"
+  },
+  {
+    "revision": "3f909f4c7b3b30ae1276",
+    "url": "/dad-jokes-app/static/js/main.ddfebb13.chunk.js"
+  },
+  {
+    "revision": "90b71edc4666f1526f11",
+    "url": "/dad-jokes-app/static/js/runtime~main.3190cab5.js"
+  }
+]);
